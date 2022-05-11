ca84b178df16d8cae57c9af35a36e3c67f8015a0
MateoFlorez <89142751+MateoFlorez@users.noreply.github.com>
2022-05-11 13:01:52 -0500

    creo setters

--- Classes/Ciclista.py
+++ Classes/Ciclista.py
@@ -22,3 +22,26 @@ class Ciclista:
     @property
     def tiempo(self):
         return(self.__tiempo)
+
+    # SETTERS
+    @nombre.setter
+    def nombre(self,nombre):
+        self.__nombre = input('Digite el nombre: ')
+
+    @edad.setter
+    def edad(self,edad):
+        if(edad < 0):
+            print('La edad no puede ser negativa.')
+        else:
+            self.__edad = int(input('Digite la edad: '))
+
+    @pais.setter
+    def pais(self,pais):
+        self.__pais = input('Digite el pais: ')
+
+    @tiempo.setter
+    def tiempo(self,tiempo):
+        if(tiempo < 0):
+            print('El tiempo debe ser en minutos y no puede ser negativo.')
+        else:
+            self.__tiempo = input('Digite el tiempo: ')
