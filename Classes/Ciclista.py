b1ca423ba42beebbb6f317b2e54c9b0f2c727d8b
MateoFlorez <89142751+MateoFlorez@users.noreply.github.com>
2022-05-11 12:53:35 -0500

    creo getters

--- Classes/Ciclista.py
+++ Classes/Ciclista.py
@@ -5,3 +5,20 @@ class Ciclista:
         self.__edad = None
         self.__pais = None
         self.__tiempo = None
+
+    # GETTERS
+    @property
+    def nombre(self):
+        return(self.__nombre)
+    
+    @property
+    def edad(self):
+        return(self.__edad)
+
+    @property
+    def pais(self):
+        return(self.__pais)
+
+    @property
+    def tiempo(self):
+        return(self.__tiempo)
