746bdd22e17713b9369536c89432ab97c5215f45
MateoFlorez <89142751+MateoFlorez@users.noreply.github.com>
2022-05-19 17:13:28 -0500

    finalizo ejercicio

--- main.py
+++ main.py
@@ -9,59 +9,6 @@ tiempos = []
 
 opcion = 1
 
-# Utilizo el SETTER
-# ciclista.nombre = ''
-# ciclista.edad = 0
-# ciclista.pais = ''
-# ciclista.equipo = ''
-# ciclista.tiempo = 0
-
-# Acceder al atributo
-# ciclistas.append(print('Digite el nombre: ') + input(ciclista.nombre))
-# ciclistas.append(print('Digite la edad: ') + input(ciclista.edad))
-# ciclistas.append(print('Digite el pais: ') + input(ciclista.pais))
-# ciclistas.append(print('Digite el equipo: ') + input(ciclista.equipo))
-# ciclistas.append(print('Digite el tiempo: ') + input(ciclista.tiempo))
-
-
-# print(ciclistas)
-
-
-
-# MENU
-# print('MENÚ DEL PROGRAMA')
-# print('1. Agregar ciclista')
-# print('2. Mostrar ciclistas')
-# print('3. Mostrar ciclista con mejor tiempo')
-# print('4. Salir')
-
-# while (opcion != 4):
-#     opcion = int(input('Digita una opción: '))
-#     if(opcion == 1):
-#         agregar = {}
-#         ## Llenando el diccionario
-#         agregar[ciclista.nombre] = input('Nombre: ')
-#         agregar[ciclista.edad] = int(input('Edad: '))
-#         agregar[ciclista.pais] = input('Pais: ')
-#         agregar[ciclista.equipo] = input('Equipo: ')
-#         agregar[ciclista.tiempo] = int(input('Tiempo: '))
-        
-#         # Llenando la lista
-#         ciclistas.append(agregar)
-
-#     elif(opcion == 2):
-#         print(ciclistas)
-
-    
-#     elif(opcion == 4):
-#         print('Saliendo del programa...')
-#     else:
-#         print('Digita una opción valida')
-
-# else:
-#     print('---------------------------------')
-#     print('Saliste del programa.')
-
 # MENU
 print('MENÚ DEL PROGRAMA')
 print('1. Agregar ciclista')
